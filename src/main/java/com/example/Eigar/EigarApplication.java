b0d5f01263fab99eb9fd50a631d6cbe42bbb2a72
aymanwaell <aymanwael@icloud.com>
2024-02-12 13:19:30 +0200

    Initial commit

---
+++ src/main/java/com/example/Eigar/EigarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eigar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EigarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Eigar/EigarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eigar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EigarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
